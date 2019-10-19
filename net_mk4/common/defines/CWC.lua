d3989697976909f32e2f8fc4042cd4c5776648f0
gyhs <42140432+gyhs@users.noreply.github.com>
2019-10-19 17:13:36 +0900

    no message

---
+++ net_mk4/common/defines/CWC.lua
@@ -0,0 +1,4 @@
+NDefines.NAir.STRATEGIC_BOMBER_NUKE_AIR_SUPERIORITY = 0.0 -- How much air superiority is needed for a tactical bomber to be able to nuke a province
+
+NDefines.NGame.START_DATE = "1987.1.1.12"
+NDefines.NGame.END_DATE = "2020.1.1.1"
