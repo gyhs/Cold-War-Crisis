a5db705feab2d16103e130617b2a9d597633ef23
gyhs <42140432+gyhs@users.noreply.github.com>
2020-02-07 22:07:19 +0900

    a

---
+++ common/defines/CWC.lua
@@ -0,0 +1,26 @@
+NDefines.NAir.STRATEGIC_BOMBER_NUKE_AIR_SUPERIORITY = 0.0 -- How much air superiority is needed for a tactical bomber to be able to nuke a province
+
+NDefines.NGame.START_DATE = "1987.1.1.12"
+NDefines.NGame.END_DATE = "2020.1.1.1"
+
+NDefines.NGraphics.MINIMUM_PROVINCE_SIZE_IN_PIXELS = 1
+NDefines.NGraphics.COUNTRY_FLAG_TEX_MAX_SIZE = 512
+NDefines.NGraphics.COUNTRY_FLAG_SMALL_TEX_MAX_SIZE = 256
+NDefines.NGraphics.COUNTRY_FLAG_STRIPE_TEX_MAX_HEIGHT = 2048
+NDefines.NGraphics.COUNTRY_FLAG_LARGE_STRIPE_MAX_HEIGHT = 8000
+NDefines.NGraphics.POLITICAL_GRID_SMALL_BOX_LIMIT = 12
+
+NDefines_Graphics.NGraphics.BORDER_WIDTH = 0.9
+NDefines_Graphics.NGraphics.PROVINCE_BORDER_FADE_NEAR = 100
+NDefines_Graphics.NGraphics.PROVINCE_BORDER_FADE_FAR = 150
+NDefines_Graphics.NGraphics.ORDER_FRONT_MAX_OFFSETS = 4
+NDefines_Graphics.NGraphics.MOON_HEIGHT = 300
+NDefines_Graphics.NGraphics.SUN_HEIGHT  = 600
+NDefines_Graphics.NGraphics.MOON_HEIGHT_WATER = 600
+NDefines_Graphics.NGraphics.SUN_HEIGHT_WATER  = 2500
+NDefines_Graphics.NGraphics.SUN_DIFFUSE_COLOR   = {0.14, 0.1, 0.6}
+NDefines_Graphics.NGraphics.SUN_INTENSITY 		= 2.0;
+NDefines_Graphics.NGraphics.MOON_DIFFUSE_COLOR  = {0.58, 0.5, 0.4}
+NDefines_Graphics.NGraphics.MOON_INTENSITY 		= 2.5;
+NDefines_Graphics.NGraphics.COUNTRY_COLOR_BRIGHTNESS_MODIFIER = 0.72
+NDefines_Graphics.NGraphics.MINIMUM_PROVINCE_SIZE_IN_PIXELS = 1
